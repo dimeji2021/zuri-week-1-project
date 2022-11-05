dc6356d07d9667138de51f177eb76cde86fc693c
Dimeji <adedimeji88@yahoo.com>
2022-11-05 10:47:51 +0100

    refactor contact section and added more style

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,84 +1,92 @@
-import React from "react";
-import { AppStyles } from "../AppStyles";
-import {
-  ButtonStyle,
-  CheckBoxStyle,
-  EmailStyle,
-  FormWrapper,
-  HeadingStyle,
-  MessageStyle,
-  NameStyle,
-  Wrapper,
-} from "./ContactStyle";
-import { Footer } from "./Footer";
+// import React from "react";
+// import { AppStyles } from "../AppStyles";
+// import {
+//   ButtonStyle,
+//   CheckBoxStyle,
+//   EmailStyle,
+//   FormWrapper,
+//   HeadingStyle,
+//   MessageStyle,
+//   NameStyle,
+//   Wrapper,
+// } from "./ContactStyle";
+// import { Footer } from "./Footer";
 
+import {
+  Button,
+  CheckBoxWrapper,
+  Email,
+  EmailBox,
+  EmailInput,
+  Header,
+  Input,
+  Label,
+  MainWrapper,
+  MessageInput,
+  MessageLabel,
+  Name,
+  Names,
+  Paragraph,
+  TickBox,
+  TickBoxLabel,
+  Wrapper2,
+  HeaderWrappper,
+  FormWrapper,
+} from "./ContactStyle";
 export const Contact = () => {
   return (
-    <>
-      <Wrapper>
-        <HeadingStyle>
-          <div className="heading">Contact Me</div>
-          <div className="SupportingText">
+    <MainWrapper>
+      <Wrapper2>
+        <HeaderWrappper>
+          <Header> Contact Us</Header>
+          <Paragraph>
             Hi there, contact me to ask me about anything you have in mind.
-          </div>
-        </HeadingStyle>
-          <FormWrapper>
-        <form>
-            <NameStyle>
-              <div className="firstname">
-                <label for="first_name">First name</label>
-                <input
-                  type="text"
-                  id="first_name"
-                  placeholder="Enter your first name"
-                  required
-                />
-              </div>
-              <div className="lastname">
-                <label for="last_name">Last name</label>
-                <input
-                  type="text"
-                  id="last_name"
-                  placeholder="Enter your last name"
-                  required
-                />
-              </div>
-            </NameStyle>
-            <EmailStyle>
-              <label for="email" className="email">
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                placeholder="yourname@email.com"
-                required
-              />
-            </EmailStyle>
-            <MessageStyle>
-              <label for="message" className="message">
-                Message
-              </label>
-              <textarea
+          </Paragraph>
+        </HeaderWrappper>
+        <FormWrapper>
+          <Names>
+            <Name>
+              <Label for="username">First name</Label>
+              <Input
                 type="text"
-                id="message"
-                placeholder="Send me a message and i'll reply you as soon as possible"
-                required
+                placeholder="Enter your first name"
+                id="username"
               />
-            </MessageStyle>
-            <CheckBoxStyle>
-              <input type="checkbox" id="privacy" value="privacy" required />
-              <label for="privacy" className="checkbox">
-                You agree to provide your data to {} who may contact you
-              </label>
-            </CheckBoxStyle>
-          <ButtonStyle>
-            <button id="btn__submit">Send message</button>
-          </ButtonStyle>
-        </form>
-          </FormWrapper>
-      </Wrapper>
-      <Footer/>
-    </>
+            </Name>
+            <Name>
+              <Label for="username">Last name</Label>
+              <Input
+                type="text"
+                placeholder="Enter your last name"
+                id="username"
+              />
+            </Name>
+          </Names>
+          <EmailBox>
+            <Email for="username">Email</Email>
+            <EmailInput
+              type="text"
+              placeholder="yourname@email.com"
+              id="username"
+            />
+          </EmailBox>
+          <div>
+            <MessageLabel for="username">Message</MessageLabel>
+            <MessageInput
+              type="text"
+              placeholder="Send me a message and I'll reply you as soon as possible..."
+              id="username"
+            />
+          </div>
+          <CheckBoxWrapper>
+            <TickBox type="checkbox" name="cheese" id="cheese"></TickBox>
+            <TickBoxLabel for="cheese">
+              You agree to providing your data to Dimeji who may contact you.
+            </TickBoxLabel>
+          </CheckBoxWrapper>
+          <Button type="button">Send message</Button>
+        </FormWrapper>
+      </Wrapper2>
+    </MainWrapper>
   );
 };
